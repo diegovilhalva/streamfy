0ebf10a5115a0ef747557f676c7e1fef2b42e966
Diego Vilhalva <diegorocha450@yahoo.com>
2025-07-11 11:01:14 -0300

    fix route

--- backend/src/index.js
+++ backend/src/index.js
@@ -20,12 +20,6 @@ app.use(express.json())
 app.use(cookieParser())
 app.use(cors({ origin: "http://localhost:5173", credentials: true }))
 
-if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname,"../frontend/dist")))
-    app.get("/cd *",(req,res) => {
-        res.sendFile(path.join(__dirname,"../frontend","dist","index.html"))
-    })
-}
 
 
 
@@ -34,6 +28,14 @@ if (process.env.NODE_ENV === "production") {
 app.use("/api/auth", authRoutes)
 app.use("/api/users", userRoutes)
 app.use("/api/chat", chatRoutes)
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname,"../frontend/dist")))
+    app.get("*",(req,res) => {
+        res.sendFile(path.join(__dirname,"../frontend","dist","index.html"))
+    })
+}
+
 
 app.listen(PORT, () => {
     console.log(`Servidor rodando na porta ${PORT}`)

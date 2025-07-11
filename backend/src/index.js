276599e38a007e1549d7b339fb3630c9e5f751a0
Diego Vilhalva <diegorocha450@yahoo.com>
2025-07-11 08:34:10 -0300

    preparing for deployment

--- backend/src/index.js
+++ backend/src/index.js
@@ -6,10 +6,12 @@ import authRoutes from "./routes/auth.route.js"
 import connectDB from "../config/db.js"
 import userRoutes from "./routes/user.route.js"
 import chatRoutes from "./routes/chat.route.js"
+import path from "path"
 connectDB()
 dotenv.config()
 const PORT = process.env.PORT || 4000
 
+const __dirname = path.resolve()
 
 const app = express()
 
@@ -18,6 +20,12 @@ app.use(express.json())
 app.use(cookieParser())
 app.use(cors({ origin: "http://localhost:5173", credentials: true }))
 
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname,"../frontend/build")))
+    app.get("*",(req,res) => {
+        res.sendFile(path.join(__dirname,"../frontend","dist","index.html"))
+    })
+}
 
 
 

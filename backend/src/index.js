4857df3b0e48a98a453407861360e3ad7fb5694d
Diego Vilhalva <diegorocha450@yahoo.com>
2025-07-11 08:49:41 -0300

    fix index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -21,7 +21,7 @@ app.use(cookieParser())
 app.use(cors({ origin: "http://localhost:5173", credentials: true }))
 
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname,"../frontend/build")))
+    app.use(express.static(path.join(__dirname,"../frontend/dist")))
     app.get("*",(req,res) => {
         res.sendFile(path.join(__dirname,"../frontend","dist","index.html"))
     })
